ec48395f6173da01e27447df35d3e9bb6870bb8a
rodrigocipriani <chopsss@gmail.com>
2017-06-18 20:45:38 -0300

    removed webpackConfigCreator

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 const configCreator = require('./configCreator');
-const webpackConfigCreator = require('./webpackConfigCreator');
 
-module.exports = { configCreator, webpackConfigCreator };
+module.exports = { configCreator };
