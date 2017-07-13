fc652db78f3d03dde4ac405b5eae39ae67e36aa8
rodrigocipriani <chopsss@gmail.com>
2017-07-12 23:56:03 -0300

    improve Themes

--- src/react-theme/DefaultTheme.jsx
+++ src/react-theme/DefaultTheme.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './normalize.css';
+import './DefaultTheme.css';
 
 // todo : remove tap delay, essential for MaterialUI to work properly
 injectTapEventPlugin();
@@ -13,9 +15,11 @@ class DefaultTheme extends Component {
     const { children } = this.props;
 
     return (
-      <MuiThemeProvider muiTheme={ getMuiTheme() }>
-        {children}
-      </MuiThemeProvider>
+      <div className="DefaultTheme">
+        <MuiThemeProvider muiTheme={ getMuiTheme() }>
+          {children}
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
