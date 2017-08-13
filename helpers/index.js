17264348462dc2b88b0da4684952c71d2f608070
rodrigocipriani <chopsss@gmail.com>
2017-08-12 21:30:39 -0300

    removed build files

--- helpers/index.js
+++
@@ -1,7 +0,0 @@
-export { default as api } from './api';
-export { default as CachedApi } from './CachedApi';
-export { default as configCreator } from './configCreator';
-export { default as makePromiseCancelable } from './makePromiseCancelable';
-export { default as storeActionTypes } from './storeActionTypes';
-export { default as storeCreator } from './storeCreator';
-export { default as reducerCreator } from './reducerCreator';
--- react/index.js
+++
@@ -1,1 +0,0 @@
-export { default as appCreator } from './appCreator';
